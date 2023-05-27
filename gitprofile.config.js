da8d4e7c0b04d7adfa11f73f537df3824cab6c79
Elie Brosset <32967983+Youplala@users.noreply.github.com>
2023-05-27 14:56:50 +0200

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -113,7 +113,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    // defaultTheme: '',
+    defaultTheme: 'default',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
